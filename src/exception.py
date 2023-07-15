4eda01d6e6ebd3e26b2085d933aa6c832a56f8de
Shreyash <shreyashmohadikar@gmail.com>
2023-07-16 02:09:55 +0530

    update

--- src/exception.py
+++ src/exception.py
@@ -20,10 +20,10 @@ class CustomException(Exception):
         return self.error_message
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    try:
-        1/0
-    except Exception as e:
-        logging.info("Divide by Zero error")
-        raise CustomException(e,sys)
+#     try:
+#         1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero error")
+#         raise CustomException(e,sys)
